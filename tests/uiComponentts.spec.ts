8cae719d2b969097ce7a2faa21091c20726b7c7c
hapsari-personal <hapsari.laksmiw@gmail.com>
2024-07-03 20:45:29 +0800

    5: checkboxes

--- tests/uiComponentts.spec.ts
+++ tests/uiComponentts.spec.ts
@@ -39,3 +39,17 @@ test.describe('Form layouts page', () => {
         expect(await usingTheGridForm.getByRole('radio', {name: 'Option1'}).isChecked()).toBeTruthy()
     })
 })
+
+test('checkboxes', async({page}) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    await page.getByRole('checkbox', {name: "Hide on click"}).uncheck({force: true})
+    await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).check({force: true})
+
+    const allBoxes = page.getByRole('checkbox')
+    for(const box of await allBoxes.all()){
+        await box.uncheck({force: true})
+        expect(await box.isChecked()).toBeFalsy()
+    }
+}) 
