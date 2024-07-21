3eb2e42175a5e87bdf06ac2a7b1adef8cb9a407f
hapsari-personal <hapsari.laksmiw@gmail.com>
2024-07-21 21:14:17 +0800

    5: Date Picker (Part 2)

--- tests/uiComponentts.spec.ts
+++ tests/uiComponentts.spec.ts
@@ -91,67 +91,129 @@ test("lists and dropdown", async ({ page }) => {
   await expect(header).toHaveCSS("background-color", "rgb(50, 50, 89)");
 
   const colors = {
-    "Light": "rgb(255, 255, 255)",
-    "Dark": "rgb(34, 43, 69)",
-    "Cosmic": "rgb(50, 50, 89)",
-    "Corporate": "rgb(255, 255, 255)",
+    Light: "rgb(255, 255, 255)",
+    Dark: "rgb(34, 43, 69)",
+    Cosmic: "rgb(50, 50, 89)",
+    Corporate: "rgb(255, 255, 255)",
   };
 
   await dropdownMenu.click();
   for (const color in colors) {
     await optionList.filter({ hasText: color }).click();
     await expect(header).toHaveCSS("background-color", colors[color]);
-    if(color != "Corporate"){
-        await dropdownMenu.click();
-    } 
+    if (color != "Corporate") {
+      await dropdownMenu.click();
+    }
   }
-})
+});
 
-test('tooltip', async({page}) => {
-    await page.getByText('Modal & Overlays').click()
-    await page.getByText('Tooltip').click()
+test("tooltip", async ({ page }) => {
+  await page.getByText("Modal & Overlays").click();
+  await page.getByText("Tooltip").click();
 
-    const toolTipCard = page.locator('nb-card', {hasText : "Tooltip Placements"})
-    await toolTipCard.getByRole('button', {name: "Top"}).hover()
+  const toolTipCard = page.locator("nb-card", {
+    hasText: "Tooltip Placements",
+  });
+  await toolTipCard.getByRole("button", { name: "Top" }).hover();
 
-    page.getByRole('tooltip') //if you have a role tooltip created
-    const tooltip = await page.locator('nb-tooltip').textContent()
-    expect(tooltip).toEqual('This is a tooltip')
-})
+  page.getByRole("tooltip"); //if you have a role tooltip created
+  const tooltip = await page.locator("nb-tooltip").textContent();
+  expect(tooltip).toEqual("This is a tooltip");
+});
 
-test('dialog box', async({page}) => {
-    await page.getByText('Tables & Data').click()
-    await page.getByText('Smart Table').click()
+test("dialog box", async ({ page }) => {
+  await page.getByText("Tables & Data").click();
+  await page.getByText("Smart Table").click();
 
-    page.on('dialog', dialog => {
-        expect(dialog.message()).toEqual('Are you sure you want to delete?')
-        dialog.accept()
-    })
+  page.on("dialog", (dialog) => {
+    expect(dialog.message()).toEqual("Are you sure you want to delete?");
+    dialog.accept();
+  });
 
-    await page.getByRole('table').locator('tr', {hasText: "mdo@gmail.com"}).locator('.nb-trash').click()
-    await expect(page.locator('table tr').first()).not.toHaveText("mdo@gmail.com")
-})
+  await page
+    .getByRole("table")
+    .locator("tr", { hasText: "mdo@gmail.com" })
+    .locator(".nb-trash")
+    .click();
+  await expect(page.locator("table tr").first()).not.toHaveText(
+    "mdo@gmail.com"
+  );
+});
 
-test('web tables', async({page}) => {
-    await page.getByText('Tables & Data').click()
-    await page.getByText('Smart Table').click()
+test("web tables", async ({ page }) => {
+  await page.getByText("Tables & Data").click();
+  await page.getByText("Smart Table").click();
 
-    // 1 get the row by any test in this row
-    const targetRow = page.getByRole('row', {name: "twitter@outlook.com"})
-    await targetRow.locator('.nb-edit').click()
-    await page.locator('input-editor').getByPlaceholder('Age').clear()
-    await page.locator('input-editor').getByPlaceholder('Age').fill('35')
-    await page.locator('.nb-checkmark').click()
+  // 1 get the row by any test in this row
+  const targetRow = page.getByRole("row", { name: "twitter@outlook.com" });
+  await targetRow.locator(".nb-edit").click();
+  await page.locator("input-editor").getByPlaceholder("Age").clear();
+  await page.locator("input-editor").getByPlaceholder("Age").fill("35");
+  await page.locator(".nb-checkmark").click();
 
-    // 2 get the row based on the value in the specific column
-    await page.locator('.ng2-smart-pagination-nav').getByText('2').click()
-    const targetRowById = page.getByRole('row', {name: "11"}).filter({has: page.locator('td').nth(1).getByText('11')})
-    await targetRowById.locator('.nb-edit').click()
-    await page.locator('input-editor').getByPlaceholder('E-mail').clear()
-    await page.locator('input-editor').getByPlaceholder('E-mail').fill('test@test.com')
-    await page.locator('.nb-checkmark').click()
-    await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com')
-    
-})
+  // 2 get the row based on the value in the specific column
+  await page.locator(".ng2-smart-pagination-nav").getByText("2").click();
+  const targetRowById = page
+    .getByRole("row", { name: "11" })
+    .filter({ has: page.locator("td").nth(1).getByText("11") });
+  await targetRowById.locator(".nb-edit").click();
+  await page.locator("input-editor").getByPlaceholder("E-mail").clear();
+  await page
+    .locator("input-editor")
+    .getByPlaceholder("E-mail")
+    .fill("test@test.com");
+  await page.locator(".nb-checkmark").click();
+  await expect(targetRowById.locator("td").nth(5)).toHaveText("test@test.com");
 
+  // 3 Test filter of the table
+  const ages = ["20", "30", "40", "200"];
 
+  for (let age of ages) {
+    await page.locator("input-filter").getByPlaceholder("Age").clear();
+    await page.locator("input-filter").getByPlaceholder("Age").fill(age);
+    await page.waitForTimeout(500);
+    const ageRows = page.locator("tbody tr");
+
+    for (let row of await ageRows.all()) {
+      const cellValue = await row.locator("td").last().textContent();
+
+      if (age == "200") {
+        expect(await page.getByRole("table").textContent()).toContain(
+          "No data found"
+        );
+      } else {
+        expect(cellValue).toEqual(age);
+      }
+    }
+  }
+});
+
+test("datepicker", async ({ page }) => {
+  await page.getByText("Forms").click();
+  await page.getByText("Datepicker").click();
+
+  const calenderInputField = page.getByPlaceholder("Form Picker");
+  await calenderInputField.click();
+
+  let date = new Date()
+  date.setDate(date.getDate() + 200)
+  const expectedDate = date.getDate().toString()
+  const expectedMonthShot = date.toLocaleDateString('En-US', {month: 'short'})
+  const expectedMonthLong = date.toLocaleDateString('En-US', {month: 'long'})
+  const expectedYear = date.getFullYear()
+  const dateToAssert = `${expectedMonthShot} ${expectedDate}, ${expectedYear}`
+
+  let calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
+  const expectedMonthAndYear = ` ${expectedMonthLong} ${expectedYear}`
+
+  while(!calendarMonthAndYear.includes(expectedMonthAndYear)){
+    await page.locator('nb-calendar-pageable-navigation [data-name="chevron-right"]').click()
+    calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
+  }
+
+  await page
+    .locator('[class="day-cell ng-star-inserted"]')
+    .getByText(expectedDate, {exact: true})
+    .click();
+  await expect(calenderInputField).toHaveValue(dateToAssert)
+});
